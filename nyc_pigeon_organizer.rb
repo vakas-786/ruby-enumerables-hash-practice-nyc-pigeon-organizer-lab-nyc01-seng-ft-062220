4e493d0ea4b06d7fb36c98a3ee324cb1bb154a25
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-15 22:39:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object ({}) do |(key, value), new_data|
+    value.each do |attributes_key, names|
+      names.each do |name|
+        if !new_data[name]
+          new_data[name] = {}
+        end
+        if !new_data[name][key]
+          !new_data[name][key] = []
+        end
+        new_data[name][key].push(attributes_key.to_s)
+    end
+  end
+end
 end
