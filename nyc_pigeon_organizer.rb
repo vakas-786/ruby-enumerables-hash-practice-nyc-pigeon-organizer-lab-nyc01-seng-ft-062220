3e3ec5f4ab91f357684d8486613274981cd0b620
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-15 22:30:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,8 @@
 def nyc_pigeon_organizer(data)
   data.each_with_object ({}) do |(key, value), new_data|
     value.each do |attributes_key, names|
+      names.each do |name|
     end
     new_data
 end
-end 
+end
